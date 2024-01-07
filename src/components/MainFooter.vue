b23e0f290e74786bdc3433ebba0c4655c1c1b92d
Sergey <Sergey202205@gmail.com>
2024-01-07 19:53:05 +0200

    add mobile footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -32,7 +32,7 @@ export default defineComponent({
 <template>
     <footer class="link-black">
         <div class="container">
-            <div class="row s-desktop" v-if="screenWidth() > 990">
+            <div class="row s-desktop" v-if="screenWidth() > 991">
                 <div class="col-xxl-3 col-lg-3">
                     <site-logo/>
                     <div class="mail">
@@ -97,30 +97,46 @@ export default defineComponent({
                     <change-language @eventSetLocale="isOpenMobileMenu = false"></change-language>
                 </div>
             </div>
-            <div class="row s-tablet" v-else-if="screenWidth() > 767">
-                <div class="col-5">
-                    <site-logo/>
-                    <div class="mail">
-                        <strong>{{ $t('common.email') }}</strong>
-                        <a href="mailto:yavorsky.team@gmail.com">yavorsky.team@gmail.com</a>
+            <div class="s-tablet" v-else-if="screenWidth() > 767">
+                <div class="s-tablet-row">
+                    <div class="s-tablet-col">
+                        <site-logo/>
+                        <div class="mail">
+                            <strong>{{ $t('common.email') }}</strong>
+                            <a href="mailto:yavorsky.team@gmail.com">yavorsky.team@gmail.com</a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-4">
-                    <div>
+                    <div class="s-tablet-col">
                         <strong class="title">{{ $t('footer.our_products') }}</strong>
                         <nav>
                             <ul>
                                 <li v-for="app in apps" :key="app.id">
-                                    <a href="#">{{ app.name }}</a>
+                                    <router-link :to="{name:'App', params:{locale:$i18n.locale, slug:app.slug}}">
+                                        {{ app.name }}
+                                    </router-link>
                                 </li>
                             </ul>
                         </nav>
                     </div>
-                    <div>
+                    <div class="s-tablet-col">
+                        <strong class="title">{{ $t('footer.follow_us') }}</strong>
+                        <social-network></social-network>
+                        <change-language @eventSetLocale="isOpenMobileMenu = false"></change-language>
+                    </div>
+                </div>
+                <div class="s-tablet-row">
+                    <div class="s-tablet-col">
+
+                    </div>
+                    <div class="s-tablet-col">
                         <strong class="title">{{ $t('footer.company') }}</strong>
                         <nav>
                             <ul>
-                                <li><a href="#">{{ $t('footer.about_company') }}</a></li>
+                                <li>
+                                    <router-link :to="{name:'Company', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.about_company') }}
+                                    </router-link>
+                                </li>
                                 <li>
                                     <router-link :to="{name:'Blogs', params:{locale:$i18n.locale}}">
                                         {{ $t('footer.blog') }}
@@ -134,9 +150,7 @@ export default defineComponent({
                             </ul>
                         </nav>
                     </div>
-                </div>
-                <div class="col-3">
-                    <div>
+                    <div class="s-tablet-col">
                         <strong class="title">{{ $t('footer.policy') }}</strong>
                         <nav>
                             <ul>
@@ -153,11 +167,6 @@ export default defineComponent({
                             </ul>
                         </nav>
                     </div>
-                    <div>
-                        <strong class="title">{{ $t('footer.follow_us') }}</strong>
-                        <social-network></social-network>
-                        <change-language @eventSetLocale="isOpenMobileMenu = false"></change-language>
-                    </div>
                 </div>
             </div>
             <div class="row s-mobile" v-else>
@@ -171,61 +180,67 @@ export default defineComponent({
                     </div>
                 </div>
                 <div class="col-6">
-                    <div>
-                        <div>
-                            <strong class="title">{{ $t('footer.our_products') }}</strong>
-                            <nav>
-                                <ul>
-                                    <li v-for="app in apps" :key="app.id">
-                                        <a href="#">{{ app.name }}</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div>
-                            <strong class="title">{{ $t('footer.company') }}</strong>
-                            <nav>
-                                <ul>
-                                    <li><a href="#">{{ $t('footer.about_company') }}</a></li>
-                                    <li>
-                                        <router-link :to="{name:'Blogs', params:{locale:$i18n.locale}}">
-                                            {{ $t('footer.blog') }}
-                                        </router-link>
-                                    </li>
-                                    <li>
-                                        <router-link :to="{name:'Contact', params:{locale:$i18n.locale}}">
-                                            {{ $t('footer.support') }}
-                                        </router-link>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
+                    <div class="col-gap-top">
+                        <strong class="title">{{ $t('footer.our_products') }}</strong>
+                        <nav>
+                            <ul>
+                                <li v-for="app in apps" :key="app.id">
+                                    <router-link :to="{name:'App', params:{locale:$i18n.locale, slug:app.slug}}">
+                                        {{ app.name }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
                 <div class="col-6">
-                    <div>
-                        <div>
-                            <strong class="title">{{ $t('footer.policy') }}</strong>
-                            <nav>
-                                <ul>
-                                    <li>
-                                        <router-link :to="{name:'PrivacyPolicy', params:{locale:$i18n.locale}}">
-                                            {{ $t('footer.privacy_policy') }}
-                                        </router-link>
-                                    </li>
-                                    <li>
-                                        <router-link :to="{name:'CookiesPolicy', params:{locale:$i18n.locale}}">
-                                            {{ $t('footer.cookies_policy') }}
-                                        </router-link>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div>
-                            <strong class="title">{{ $t('footer.follow_us') }}</strong>
-                            <social-network/>
-                            <change-language @eventSetLocale="isOpenMobileMenu = false"></change-language>
-                        </div>
+                    <div class="col-gap-top">
+                        <strong class="title">{{ $t('footer.follow_us') }}</strong>
+                        <social-network/>
+                        <change-language @eventSetLocale="isOpenMobileMenu = false"></change-language>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="col-gap-top">
+                        <strong class="title">{{ $t('footer.company') }}</strong>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <router-link :to="{name:'Company', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.about_company') }}
+                                    </router-link>
+                                </li>
+                                <li>
+                                    <router-link :to="{name:'Blogs', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.blog') }}
+                                    </router-link>
+                                </li>
+                                <li>
+                                    <router-link :to="{name:'Contact', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.support') }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="col-gap-top">
+                        <strong class="title">{{ $t('footer.policy') }}</strong>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <router-link :to="{name:'PrivacyPolicy', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.privacy_policy') }}
+                                    </router-link>
+                                </li>
+                                <li>
+                                    <router-link :to="{name:'CookiesPolicy', params:{locale:$i18n.locale}}">
+                                        {{ $t('footer.cookies_policy') }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -265,10 +280,56 @@ footer
             font-size: 14px
             padding: 6px 0
 
+    .social-network
+        padding: 0 0 45px
+
+    .s-desktop
         li:first-child a
             padding-top: 0
 
-    .social-network
-        padding: 0 0 45px
+    .s-tablet
+        .title
+            margin-bottom: 8px
+
+        .logo
+            padding-bottom: 8px
+
+        .social-network
+            padding-bottom: 16px
+
+        a
+            padding: 4px 0
+
+    .s-tablet-row
+        display: flex
+        justify-content: space-between
+        padding-bottom: 16px
+        width: 100%
+
+        &:last-child
+            padding-bottom: 0
+
+        .s-tablet-col
+            &:nth-child(1)
+                flex: 40%
+
+            &:nth-child(2), &:nth-child(3)
+                flex: 30%
+
+    .s-mobile
+        .logo
+            padding-bottom: 8px
+
+        a
+            padding: 4px 0
+
+        .title
+            margin-bottom: 8px
+
+        .social-network
+            padding-bottom: 30px
+
+        .col-gap-top
+            padding-top: 16px
 
 </style>
